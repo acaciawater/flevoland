45a6a4c381dbf3f739cbcc7cf27843542e98be79
Stefan Korenberg <stefan.korenberg@acaciawater.com>
2017-11-16 08:20:04 +0100

    Piezometers toegevoegd (koppeltabel tussen rectangle-id en reeks)

---
+++ flevoland/models.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from django.db import models
+from acacia.data.models import Series, MeetLocatie
+
+class Piezometer(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    description = models.TextField(blank=True, null=True, verbose_name='omschrijving')
+    meetlocatie = models.ForeignKey(MeetLocatie)
+    series = models.ForeignKey(Series, verbose_name='reeks')
+    rectangle_id = models.CharField(max_length=50, blank=True, null=True, verbose_name='rectangle ID', help_text='ID van de bijbehorende SVG rectangle')
+
+    def __unicode__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'piezometers'
--- flevoland/views.py
+++ flevoland/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import get_object_or_404
 from django.views.generic.base import TemplateView
 from django.views import generic
 
-from acacia.data.models import Project, TabGroup, ProjectLocatie
+from acacia.data.models import Project, TabGroup, ProjectLocatie, MeetLocatie
 from acacia.data.views import ProjectDetailView
 
 class HomeView(ProjectDetailView):
@@ -20,23 +20,66 @@ class HomeView(ProjectDetailView):
         context = super(HomeView, self).get_context_data(**kwargs)
         context['maptype'] = 'roadmap'
         return context
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 class LocationView(generic.DetailView):
     model = ProjectLocatie
     
+    def get_latest_values(self):
+        meetlocatie = get_object_or_404(MeetLocatie,name='Perseel N')
+        latest_values =  [{'rectangle_id' : x.rectangle_id, 'level':x.series.laatste().value} for x in meetlocatie.piezometer_set.all()]
+        return latest_values
+    
     def get_context_data(self, **kwargs):
         context = super(LocationView, self).get_context_data(**kwargs)
-        context['test'] = 'test'
+        context['latest'] = self.get_latest_values()
         return context
     
     template_name = 'project_locatie_detail.html'
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
 class FirstDetailView(generic.DetailView):
     model = ProjectLocatie
     
     def get_context_data(self, **kwargs):
         context = super(FirstDetailView, self).get_context_data(**kwargs)
-        context['test'] = 'test'
         return context
     
     template_name = 'details1.html'
@@ -46,7 +89,6 @@ class SecondDetailView(generic.DetailView):
     
     def get_context_data(self, **kwargs):
         context = super(SecondDetailView, self).get_context_data(**kwargs)
-        context['test'] = 'test'
         return context
     
     template_name = 'details2.html'
