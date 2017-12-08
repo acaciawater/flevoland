d455cba72b671ab0db3a913fd40f98395827cb4a
Stefan Korenberg <stefan.korenberg@acaciawater.com>
2017-12-08 09:53:42 +0100

    cache the location page for 15 minutes

--- flevoland/urls.py
+++ flevoland/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
+from django.views.decorators.cache import cache_page
 
 from .views import HomeView, LocationView, FirstDetailView, SecondDetailView, history_JS, info
 from acacia.data.views import DashGroupView
@@ -15,7 +16,7 @@ urlpatterns = [
     url(r'^data/', include('acacia.data.urls',namespace='acacia')),
     url(r'^detail1/(?P<pk>[0-9]+)/$', FirstDetailView.as_view(), name='detail1'),
     url(r'^detail2/(?P<pk>[0-9]+)/$', SecondDetailView.as_view(), name='detail2'),
-    url(r'^(?P<pk>[0-9]+)/$', LocationView.as_view(), name='location'),
+    url(r'^(?P<pk>[0-9]+)/$', cache_page(60*15)(LocationView.as_view()), name='location'),
     url(r'^history/(?P<pk>[0-9]+)/$', history_JS, name='history'),
     url(r'^info$', info, name='info')
 ]
